9c4a6c8ae5b57d0235a3869e0fd1afe625d9e007
rogerdpack <rogerpack2005@gmail.com>
2011-10-18 21:29:29 -0600

    delete old files if the disk gets filled up...and try to keep processing while doing it

--- go.rb
+++ go.rb
@@ -10,20 +10,41 @@ def generate_preview_image from_this
 	raise unless File.exist? to_file
 end
 
+require 'fileutils'
+Thread.abort_on_exception=true # sanity
+
+
+def delete_if_out_of_disk_space
+    require 'java' # jruby <sigh>
+  
+    free_space = java.io.File.new('.').freeSpace
+  
+    ten_gig = 10e9
+    if free_space < ten_gig
+	  # lodo email instead?
+	  $deletor_thread ||= Thread.new {
+        dirs = Dir['captured_video/*/*']
+	    oldest_dir = dirs.min_by{|name| name.split('/')[2]}
+	    p 'deleting old day dir ' + oldest_dir
+	    FileUtils.rm_rf oldest_dir
+		$deletor_thread = nil # let next guy through delete something too...
+	  }
+  end
+end
+
 all_cameras = {'eyeball' => [0, '1280x1024'], 'thin_camera' => [1,'1280x960']}
 
 all_cameras.each{|camera_name, (index, resolution)|
   Thread.new {
   loop {
-  
   current = Time.now
   current_file_timestamp = current.strftime "%H-%Mm"
-  p "doing #{current_file_timestamp} #{camera_name}"
   sixty_minutes = 60*60
-  #sixty_minutes = 20 #seconds
+  sixty_minutes = 10 #seconds
   raise 'unexpected' if camera_name =~ / /
   bucket_day_dir = 'captured_video/' + camera_name + '/' + current.strftime("%Y-%m-%d")
   FileUtils.mkdir_p bucket_day_dir
+  p "doing #{bucket_day_dir}/#{current_file_timestamp} #{camera_name}"
   
   # todo motion detect :P
   # todo smallify x days worth
@@ -44,13 +65,13 @@ all_cameras.each{|camera_name, (index, resolution)|
   # TODO no -y ...
   c = %!ffmpeg\\ffmpeg -y #{input} -vcodec mpeg4 -t #{sixty_minutes} -r 5 "#{filename}"  2>&1!
   
-  puts c
+  #puts c
   out_handle = IO.popen(c)
   `.\\SetPriority.exe -lowest #{out_handle.pid}`
   raise unless $?.exitstatus == 0
-  stdout = out_handle.read
+  output = out_handle.read
   generate_preview_image filename
-  
+  delete_if_out_of_disk_space
  }
  }
 }
