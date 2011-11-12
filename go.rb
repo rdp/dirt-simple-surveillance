3d3ad5160303cb2011391ac489101e9b458cfe5b
rogerdpack <rogerpack2005@gmail.com>
2011-11-12 15:34:23 -0700

    better ordering, avoid race cond

--- go.rb
+++ go.rb
@@ -13,25 +13,31 @@ end
 
 require 'fileutils'
 Thread.abort_on_exception=true # sanity
+require 'thread'
 
+$thread_start = Mutex.new
 
 def delete_if_out_of_disk_space
     require 'java' # jruby <sigh>
   
     free_space = java.io.File.new('.').freeSpace
   
-    delete_if_we_have_less_than_this_much_free_space = 45e9
+    delete_if_we_have_less_than_this_much_free_space = 55e9
     if free_space < delete_if_we_have_less_than_this_much_free_space
-	  # lodo email instead?
-	  # possible race condition ||= LOL
-	  $deletor_thread ||= Thread.new {
-	    
-        dirs = Dir['captured_video/*/*']
-	    oldest_dir = dirs.min_by{|name| name.split('/')[2]}
-	    p "deleting old day dir #{oldest_dir} because #{free_space} free < #{delete_if_we_have_less_than_this_much_free_space}"
-	    FileUtils.rm_rf oldest_dir
-		$deletor_thread = nil # let next guy through delete if more should be deleted...
+	  # lodo email instead? compact?
+	  $thread_start.synchronize {
+		  $deletor_thread ||= Thread.new {
+			
+			dirs = Dir['captured_video/*/*']
+			oldest_dir = dirs.min_by{|name| name.split('/')[2]}
+			p "deleting old day dir #{oldest_dir} because free #{free_space} < #{delete_if_we_have_less_than_this_much_free_space}"
+			FileUtils.rm_rf oldest_dir
+			p "done deleting"
+			$deletor_thread = nil # let next guy through delete if more should be deleted...
+		  }
 	  }
+  else
+    puts "have enough free space #{free_space/1_000_000_000}G"
   end
 end
 
@@ -40,15 +46,7 @@ all_cameras = {'eyeball' => [0, '1280x1024'], 'thin_camera' => [1,'1280x960']}
 all_cameras.each{|camera_name, (index, resolution)|
   Thread.new {
   loop {
-  current = Time.now
-  current_file_timestamp = current.strftime "%H-%Mm"
-  sixty_minutes = 60*60
-  #sixty_minutes = 10 #seconds
-  raise 'unexpected' if camera_name =~ / /
-  bucket_day_dir = 'captured_video/' + camera_name + '/' + current.strftime("%Y-%m-%d")
-  FileUtils.mkdir_p bucket_day_dir
-  p "doing #{bucket_day_dir}/#{current_file_timestamp}"
-  
+
   # todo motion detect :P
   # todo smallify x days worth
   # todo make config ridiculously easy LOL
@@ -59,10 +57,20 @@ all_cameras.each{|camera_name, (index, resolution)|
   #input = "-i tee.avs"
   
   if ARGV.detect{|a| a == '--preview'}
-    c = %!ffmpeg\ffplay #{input}!
+    c = %!ffmpeg\\ffplay #{input}!
+	puts c
     system c
     exit
   end
+  delete_if_out_of_disk_space
+  current = Time.now
+  current_file_timestamp = current.strftime "%H-%Mm"
+  sixty_minutes = 60*60
+  #sixty_minutes = 10 #seconds
+  raise 'unexpected' if camera_name =~ / /
+  bucket_day_dir = 'captured_video/' + camera_name + '/' + current.strftime("%Y-%m-%d")
+  FileUtils.mkdir_p bucket_day_dir
+  p "doing #{bucket_day_dir}/#{current_file_timestamp}"
   filename = "#{bucket_day_dir}/#{current_file_timestamp}.mp4"
     
   # TODO no -y ...
@@ -74,7 +82,6 @@ all_cameras.each{|camera_name, (index, resolution)|
   raise unless $?.exitstatus == 0
   output = out_handle.read
   generate_preview_image filename
-  delete_if_out_of_disk_space
  }
  }
 }
