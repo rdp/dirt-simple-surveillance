991a0a83c9db93e28ada8ef6da5a5a608ad92e1a
rogerdpack <rogerpack2005@gmail.com>
2011-12-05 09:36:00 -0700

    simplify

--- repro.rb
+++ repro.rb
@@ -1,16 +1,3 @@
-$count = 1
-def lowify
-  # avoid WMI which apparently leaks
-  piddys = `tasklist`
-
-end
-
-Thread.new { 
-
-loop { lowify } 
-
-}
-
+Thread.new { loop { `tasklist` } }
 
 gets
-system("taskkill /f /im ffmpeg*")
