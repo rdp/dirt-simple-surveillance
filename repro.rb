8bb788d28eb4d08e904b6af114a018bd7c5bc2e6
rogerdpack <rogerpack2005@gmail.com>
2011-12-05 09:00:21 -0700

    still there

--- repro.rb
+++ repro.rb
@@ -7,14 +7,10 @@ end
 
 Thread.new { 
 
-  out_handle = IO.popen('ls')
-
 loop { lowify } 
 
 }
 
 
-sleep 1 # make this show up lower on the display console
-puts 'hit enter to quit/cancel current vid'
 gets
 system("taskkill /f /im ffmpeg*")
