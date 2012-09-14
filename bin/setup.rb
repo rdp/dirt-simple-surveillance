e255f318b1f01d2464cb44a22507e850d8abb5ab
rogerdpack <rogerpack2005@gmail.com>
2012-09-14 16:35:52 -0600

    should work

--- bin/setup.rb
+++ bin/setup.rb
@@ -1,12 +1,33 @@
 require 'shared'
-a = SimpleGuiCreator::ParseTemplate.new.parse_setup_filename('lib\\setup.sgc')
+Storage
+include SimpleGuiCreator
+a = ParseTemplate.new.parse_setup_filename('lib\\setup.sgc')
 
+old_existing = UsbStorage['devices_to_record']
+incoming_string = %! "Currently recording to the following devices:" !
+p old_existing
+if(old_existing.size == 0)
+ incoming_string += " (none yet, add one!):"
+end
+
+a.add_setup_string_at_bottom  incoming_string
+old_existing.each{|device_name, name|
+  add_device device_name, name, a
+}
+
+def add_device device_name, english_name, to_this
+  to_this.add_setup_string_at_bottom %! "#{device_name} => #{name}" !
+end
+
+a.set_size 500,500 # TODO not have to do this...
 a.elements[:add_new_url].on_clicked {
-  puts 'clicked url'
   SimpleGuiCreator.display_text "not implemented yet!"
 }
 
 a.elements[:add_new_local].on_clicked {
- devices = FfmpegHelpers.enumerate_directshow_devices[:video]
- p devices
+ video_devices = FfmpegHelpers.enumerate_directshow_devices[:video]
+ new_name = DropDownSelector.new(nil, video_devices, "Select video device to capture").go_selected_value
+ english_name = SimpleGuiCreator.get_input "Please enter the 'alias' name you'd like to have (human friendly name) for #{new_name}:", new_name
+ UsbStorage['devices_to_record'][new_name] =  UsbStorage['devices_to_record'][new_name] || english_name
+ add_device new_name, english_name, a
 }
--- lib/shared.rb
+++ lib/shared.rb
@@ -4,7 +4,10 @@ require 'simple_gui_creator'
 ENV['PATH'] = 'vendor\\ffmpeg;vendor;' + ENV['PATH'] # put our ffmpeg first, see jruby#6211
 require 'simple_gui_creator'
 
+UsbStorage = Storage.new('dirt_simple_usb_storage')
+UsbStorage.set_default('devices_to_record', {})
+
 def get_sorted_day_dirs
-			dirs = Dir['captured_video/*/*']
-			dirs.sort_by{|name| name.split('/')[2]}
+	dirs = Dir['captured_video/*/*']
+	dirs.sort_by{|name| name.split('/')[2]}
 end
