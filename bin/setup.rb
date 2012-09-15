8c6ceb697d663a021c22ef5bcbf3bd1dd8f0b3a7
rogerdpack <rogerpack2005@gmail.com>
2012-09-15 16:25:01 -0600

    better remove functionality

--- bin/setup.rb
+++ bin/setup.rb
@@ -12,27 +12,33 @@ end
 @device_count = 0
 
 def add_device device_name, english_name, to_this
+  UsbStorage['devices_to_record'][device_name] = english_name
+  UsbStorage['devices_to_record'] = UsbStorage['devices_to_record'] # force save [hmm...]
+  
   init_string = "#{english_name}"
   if device_name != english_name
     init_string += " (#{device_name})"
   end
 
-  init_string = '"' + init_string + '"'
-  my_remove_button_name = "remove_#{@device_count += 1}"
+  unique_number = @device_count += 1
+  init_string = '"' + init_string + ":name_string_#{unique_number}\""
+  my_remove_button_name = "remove_#{unique_number}"
   init_string += "[Remove:#{my_remove_button_name}]"
   to_this.add_setup_string_at_bottom init_string
   to_this.elements[my_remove_button_name.to_sym].on_clicked {
     UsbStorage['devices_to_record'].delete(device_name)
 	UsbStorage['devices_to_record'] = UsbStorage['devices_to_record'] # force save
-	SimpleGuiCreator.display_text "please run it again to refresh the list"
-	to_this.close
+	p to_this.elements
+	to_this.elements[:"name_string_#{unique_number}"].text = 'removed it!'
   }
+  
   to_this.set_size 350,450 # TODO not have to do this...
 end
 
 old_existing.each{|device_name, name|
   add_device device_name, name, a
 }
+
 a.set_size 350,450 # TODO not have to do this...
 
 a.elements[:add_new_url].on_clicked {
@@ -43,8 +49,13 @@ a.elements[:add_new_local].on_clicked {
  video_devices = FfmpegHelpers.enumerate_directshow_devices[:video]
  new_name = DropDownSelector.new(nil, video_devices, "Select video device to capture").go_selected_value
  english_name = SimpleGuiCreator.get_input "Please enter the 'alias' name you'd like to have (human friendly name) for #{new_name}:", new_name
- UsbStorage['devices_to_record'][new_name] =  UsbStorage['devices_to_record'][new_name] || english_name
- UsbStorage['devices_to_record'] =  UsbStorage['devices_to_record'] # force save [hmm...]
+
+ video_fps_options = FfmpegHelpers.get_options_video_device new_name
+ # like  {:video_type=>"vcodec", :video_type_name=>"mjpeg", :min_x=>"800", :max_x=>"800", :max_y=>"600", "30"=>"30"}
+ 
+ # now for a huge list...
+# displayable = 
+ 
  add_device new_name, english_name, a
  # TODO frame rate/size etc. options :)
 }
