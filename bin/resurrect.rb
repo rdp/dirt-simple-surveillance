011fc391370672876f4a12c75328600428fe3416
rogerdpack <rogerpack2005@gmail.com>
2012-10-03 21:43:15 -0600

    2 promptable

--- bin/resurrect.rb
+++ bin/resurrect.rb
@@ -7,7 +7,12 @@ if File.exist? currently_running_filename
   if File.exist? currently_hidden_filename
     require 'fileutils'
     FileUtils.touch wakeup_filename
-    puts 'it should be coming up, if running at all...'
+    if SimpleGuiCreator.show_select_buttons_prompt('it should be coming up, if running at all... run anyway?') == :yes
+	  FileUtils.rm_rf currently_hidden_filename
+	else
+	  exit 1
+	end
+    puts 
     exit 0
   else
     if SimpleGuiCreator.show_select_buttons_prompt("possibly already running in other window, run anyway?") == :yes
